1a05a2968133ee1c0f72bfcd2651324a3a56d2ce
Chadly Riedewald <no925.c@gmail.com>
2022-05-11 17:08:21 +0200

    added redirect for netlify

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,12 +1,17 @@
+import { useNavigate } from 'react-router-dom';
+
 import {
     DirectoryItemContainer,
     BackgroundImg,
     Body,
 } from './directory-item.styles';
 
-const DirectoryItem = ({ category: { imageUrl, title } }) => {
+const DirectoryItem = ({ category: { imageUrl, title, route } }) => {
+    const navigate = useNavigate();
+
+    const onNavigateHandler = () => navigate(route);
     return (
-        <DirectoryItemContainer>
+        <DirectoryItemContainer onClick={onNavigateHandler}>
             <BackgroundImg imageUrl={imageUrl} />
             <Body>
                 <h2>{title}</h2>
